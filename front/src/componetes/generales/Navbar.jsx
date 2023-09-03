c0a2b1a813354541f1ab2f7fa9d7b26ec6be16bd
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-09-03 10:29:56 -0500

    version login y maquetado mejorada

--- front/src/componetes/generales/Navbar.jsx
+++ front/src/componetes/generales/Navbar.jsx
@@ -1,26 +1,24 @@
-
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
 function Menu() {
   return (
-        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" className="mb-3">
-          <Container>
-            <Navbar.Brand href="#" className="fs-3">TASKMANAGER</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="ms-auto">
-                <Link to="/inicio" className="nav-link fs-4">Inicio</Link>
-                <Link to="/about" className="nav-link fs-4">Acerca de</Link>
-              </Nav>
-              <Nav>
-                <Button variant="success" as={Link} to="/login" className="fs-4">Login</Button>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+<Navbar collapseOnSelect expand="lg" bg="light" variant="light" className="mb-3">
+  <Container>
+    <Navbar.Brand  className="fs-3 text-primary fw-bold">TASKMANAGER</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav ">
+      <Nav className="ms-auto text-center fw-bold">
+        <Link to="/inicio" className="nav-link fs-md-4 fs-md-3 text-primary">Inicio</Link>
+        <Link to="/about" className="nav-link fs-md-4 fs-sm-3 text-primary">Acerca de</Link>
+      </Nav>
+      <Nav className="d-flex align-items-center">
+        <Button variant="primary" as={Link} to="/login" className="fs-md-4 fs-sm-3 fw-bolder">Login</Button>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
       );
     }
     
--- front/src/pages/Inicio.jsx
+++ front/src/pages/Inicio.jsx
@@ -1,21 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Button, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
-function Home() {
+const Home = () => {
   return (
-<div className="mt-5">
-  <div className="bg-light text-black text-center py-5 mt-3rem">
-    <h1 className="text-xl font-bold">TaskManager</h1>
-    <p className="text-xl mt-4">Mantente organizado y productivo</p>
-    <Link to="/Registro" className="mt-4">
-      <button className="btn btn-dark mt-4">Empezar</button>
-    </Link>
-    <div className="text-black text-center py-3 mt-8">
-      <p className="font-semibold text-xl mb-0">Task Manager &copy; 2023</p>
+    <div className="mt-5">
+      <div className="bg-primary text-white text-center py-5 mt-3rem">
+        <FontAwesomeIcon icon={faList} size="4x" className="mb-3" />
+        <h1 className="text-xl font-bold">TaskManager</h1>
+        <p className="text-xl mt-4">Mantente organizado y productivo</p>
+        <Link to="/Registro" className="mt-4">
+          <Button variant="light" className="mt-4">Empezar</Button>
+        </Link>
+        <div className="text-white text-center py-3 mt-8">
+          <p className="font-semibold text-xl mb-0">Task Manager &copy; 2023</p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
-}
+};
 
 export default Home;
